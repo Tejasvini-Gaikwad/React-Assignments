55e05e362fada10de1e6c0edf5e504e520cd1f76
Tejasvini Gaikwad <tejasvini.gaikwad@joshsoftware.com>
2023-01-10 23:06:33 +0530

    react assignment with usefetch

---
+++ to-do-list/src/ListItem.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import Badge from 'react-bootstrap/Badge';
+import Button from 'react-bootstrap/Button';
+
+const ListItem = ({item, updateCompleted, deleteCompletedItems,setIds, ids, setChecked}) => {
+   
+    
+    const handleChange = (e) => {
+        let updatedList = [...ids];
+        if(e.target.checked){
+            updatedList = [...ids,parseInt(e.target.value)];
+        }else{
+            updatedList.splice(ids.indexOf(e.target.value), 1);
+        }
+        setIds(updatedList);
+        if(updatedList.length > 0){
+            setChecked("");
+        }else{
+            setChecked("disabled");
+        }
+    }
+    return <>
+        <td><input type="checkbox" id={item.id} name={item.name} value={item.id} onChange={handleChange} /></td>
+        <td>{item.name}</td><td>{item.completed? <Badge bg="success">Completed</Badge> : <Badge bg="warning">Incomplete</Badge>}</td>
+        <td>{!item.completed ? <Button variant="info" onClick={()=>{updateCompleted([item.id])}}>Mark as Complete</Button>:""}{" "}<Button variant="danger" onClick={()=>{deleteCompletedItems([item.id])}}>Delete</Button></td>
+    </>
+}
+
+export default ListItem;

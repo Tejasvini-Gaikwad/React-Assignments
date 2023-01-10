5894e400b74a7ff8f3124d59642800537c6f0316
Tejasvini Gaikwad <tejasvini.gaikwad@joshsoftware.com>
2023-01-10 23:12:27 +0530

    react routers

---
+++ to-do-list/src/Todo.js
@@ -0,0 +1,62 @@
+    import React,{ useEffect, useState } from "react";
+    import useFetch from "react-fetch-hook";
+    import Table from 'react-bootstrap/Table';
+    import Button from 'react-bootstrap/Button';
+    import Badge from 'react-bootstrap/Badge';
+    import AddTodo from "./AddTodo";
+    import ListTodo from "./ListTodo";
+
+    const Todo = ({list,setList}) =>{
+        const {isLoading, data, error} = useFetch("http://localhost:8000/Todos");
+      
+        useEffect(()=>{
+          if(list === undefined || list.length ===0){
+            setList(data);
+          }
+        }, [data])
+       
+        const deleteCompletedItems = (ids) => {
+            const newArr = list.filter((data)=>{
+              if(!ids.includes(data.id)){
+                return data;
+              }
+            })
+            setList(newArr);
+        }
+
+         //function to update single item
+        const updateCompleted = (ids) => {
+            const newArrUpdate = list.map(data => {
+            if (ids.includes(data.id)) {
+                return {
+                ...data,
+                completed: true,
+                };
+            } else {
+                return data;
+            }
+            });
+            setList(newArrUpdate)
+        }
+
+
+        if(error){
+            return <div><br />{error.message}<div id="loading" style={{"align":"center"}}>
+                <img id="loading-image" src="loader.gif" alt="Loading..."/>
+            </div></div>
+        }
+        if(isLoading){
+            return <>
+                <br /><div id="loading" style={{"align":"center"}}>
+                        <img id="loading-image" src="loader.gif" alt="Loading..."/>
+                </div>
+            </>
+        }
+        return (
+            <>
+                {list && <ListTodo list={list} updateCompleted={updateCompleted} deleteCompletedItems={deleteCompletedItems}/>}
+            </>
+        )
+    }
+
+    export default Todo;
